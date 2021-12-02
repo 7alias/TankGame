964d6eb4ec124ad53d6aa7f7f014c982fe3e80c5
7alias <xc.navi@gmail.com>
2021-12-02 10:19:25 +0200

    Ready to play

--- src/Tank.java
+++ src/Tank.java
@@ -1,9 +1,9 @@
 public class Tank {
-
-    int x, y = 0;
+    int x;
+    int y;
     int dir;
     int fuel;
-    int r;
+
 
     public Tank(int x, int y, int f) {
         this.x = x;
@@ -12,42 +12,26 @@ public class Tank {
     }
 
     public Tank(int x, int y) {
-        this.x = x;
-        this.y = y;
-        this.fuel = 100;
+        this(x, y, 100);
     }
 
     public Tank() {
-        x=0;
-        y=0;
-        this.fuel = 100;
+        this(0, 0, 100);
+
     }
 
     public void goForward(int i) {
-
-        int w = getFuel(this.fuel, i);
-
-        if (dir == 0) {
-            x += w;
-
-        } else if (dir == 1) {
-            y += w;
-
-        } else if (dir == 2) {
-
-            x -= w;
-
-        } else {
-
-            y -= w;
-
-
-        }
+        if (i < 0 && -i > fuel) i = -fuel;
+        else if (i > fuel) i = fuel;
+        if (dir == 0) x += i;
+        else if (dir == 1) y += i;
+        else if (dir == 2) x -= i;
+        else y -= i;
+        fuel -= Math.abs(i);
     }
 
-
     public void printPosition() {
-        System.out.println("The Tank is at " + x + ", " + y + " now. ");
+        System.out.println("The Tank is at " + x + ", " + y + " now.");
     }
 
     public void turnLeft() {
@@ -62,27 +46,5 @@ public class Tank {
 
     public void goBackward(int i) {
         goForward(-i);
-    }
-
-    public int getFuel(int fuel, int i) {
-
-
-
-        if (i < this.fuel) {
-            if (i<0){i =i*-1;}
-            r = i;
-            this.fuel -= r;
-
-
-        } else if (i> this.fuel) {
-            r = this.fuel;
-            this.fuel = 0;
-
-        } else {
-            this.r = this.fuel;
-            this.fuel = this.fuel -i;
-        }
-
-        return r;
     }
 }
